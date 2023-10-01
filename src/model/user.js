f616c0bf152a8f5a6cc1324d69a1628fd6068822
jwooo <parkjwoo0515@gmail.com>
2023-10-01 21:07:33 +0900

    feat: User 클래스 삭제

--- src/model/user.js
+++
@@ -1,7 +0,0 @@
-export default class User {
-    constructor(email, password, nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-    }
-}
