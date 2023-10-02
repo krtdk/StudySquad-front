60a0ca571276e55a2f8a9e3d2eb061493ec12c2f
jwooo <parkjwoo0515@gmail.com>
2023-10-02 22:46:36 +0900

    feat: SquadRecruitTable 목록 라우팅 기능 추가

--- src/views/board/SquadRecruitTable.vue
+++ src/views/board/SquadRecruitTable.vue
@@ -16,13 +16,20 @@
           class="table_row"
           @click="routeRecruitSquad(item)"
       >
-        <td>{{item.squadName}}</td>
-        <td>{{item.categoryName}}</td>
-        <td>{{item.creatorName}}</td>
-        <td>{{item.userCount}} / 4</td>
+        <td>{{ item.squadName }}</td>
+        <td>{{ item.categoryName }}</td>
+        <td>{{ item.creatorName }}</td>
+        <td>{{ item.userCount }} / 4</td>
       </tr>
       </tbody>
     </v-table>
+    <div class="squad_list_button_container">
+      <v-btn
+          class="squad_list_button"
+          @click="routeRecruitSquadListBoard()">
+        목록
+      </v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -30,6 +37,17 @@
 .table_row:hover {
   background-color: #f5f5f5;
   cursor: pointer;
+}
+
+.squad_list_button_container {
+  padding-top: 10px;
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.squad_list_button {
+  background-color: #8580D8F5;
+  color: black;
 }
 </style>
 
@@ -42,6 +60,7 @@ export default {
   data() {
     return {
       data: [],
+      mentor: '',
     }
   },
 
@@ -49,8 +68,6 @@ export default {
     handleRecruitSquad() {
       SquadService.getRecruitSquads(this.params)
           .then(response => {
-            console.log(response);
-            console.log(this.params);
             this.data = response.data.content;
           })
           .catch(() => {
@@ -59,16 +76,24 @@ export default {
     },
     routeRecruitSquad(item) {
       console.log(item);
-      // const routeItem = item;
 
       this.$router.push({
         path: `/squad/recruit/${item.squadId}`,
         state: item,
       });
+    },
+    routeRecruitSquadListBoard() {
+      this.$router.push({
+        path: '/squads',
+        state: {
+          mentor: this.mentor,
+        },
+      });
     }
   },
   created() {
     this.handleRecruitSquad();
+    this.mentor = this.params.mentor;
   }
 }
 </script>
