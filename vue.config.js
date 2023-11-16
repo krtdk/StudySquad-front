b6ac91d0c78a1378ab08b9e23e3fe8c2aac5413f
jwooo <parkjwoo0515@gmail.com>
2023-11-16 14:28:18 +0900

    fix: vue.config.js 수정

--- vue.config.js
+++ vue.config.js
@@ -2,25 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
  transpileDependencies: true,
 
-  //outputDir: "../vue-backboard/src/main/resources/static",
-
- /* devServer: {
-    proxy: {
-      '/': {
-        target: "http://localhost:8081",
-        changeOrigin: true,
-      }
-    }
-  }, */
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
     svgRule.use('vue-svg-loader').loader('vue-svg-loader');
   },
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
 })
